d06b3261ae73e4994b30872d0a7c8a04e48221c8
Jibril <jibrilf@lmtlss.dev>
2022-06-03 21:42:54 -0500

    Added display from memory.

--- src/main.c
+++ src/main.c
@@ -4,11 +4,19 @@
 #include "../include/main.h"
 
 // Defining defaults and memory space for map state
-int cursorlocx=0;
-int cursorlocy=0;
+int cursor_x=0;
+int cursor_y=0;
 int seed[2000][2000];
+int current_state[2000][2000];
 
 void read_file(char file_name[],int MAXWIDTH) {
+	// Set seed array to nothing
+	for (int i=0; i<2000; ++i) {
+		for (int j=0; j<2000; ++j) {
+			seed[i][j] = 0;
+			current_state[i][j] = 0;
+		}
+	}
 	// Set file pointer and character array to store data
 	FILE* fptr;
 	fptr = fopen(file_name, "r");
@@ -18,8 +26,21 @@ void read_file(char file_name[],int MAXWIDTH) {
 		printf("The file is not opened.");
 	} else {
 		// Read each line
+		int i=0;
 		while (fgets(data, MAXWIDTH, fptr)) {
 			printw("%s", data);
+			for (int j=0; j<sizeof(data); ++j) {
+				if (data[j]=='X') {
+					seed[i][j]=1;
+				}
+				// Make sure everything else is set to 0 otherwise max value from last row gets carried over.
+				if (j==sizeof(data)-1) {
+					for (int k=sizeof(data); k<MAXWIDTH; ++k) {
+						seed[i][k]=0;
+					}
+				}
+			}
+			i++;
 		}
 	}
 }
@@ -44,31 +65,67 @@ int main() {
 	read_file("default_seed", WINDMAXX);
 	move(0,0);
 	refresh();
-	getch();
     //ch = getch();           /* If raw() hadn't been called we have to press enter before it * gets to the program      */
 	while ((ch = getch()) != 'q') {
 		switch (ch) {
 			case KEY_UP:
-				cursorlocx--;
+				cursor_x--;
 				break;
 			case KEY_DOWN:
-				cursorlocx++;
+				cursor_x++;
 				break;
 			case KEY_RIGHT:
-				cursorlocy++;
+				cursor_y++;
 				break;
 			case KEY_LEFT:
-				cursorlocy--;
+				cursor_y--;
+				break;
+			case 'k':
+				cursor_x--;
+				break;
+			case 'j':
+				cursor_x++;
+				break;
+			case 'l':
+				cursor_y++;
+				break;
+			case 'h':
+				cursor_y--;
 				break;
 			case 'x':
-				printw("X");
+				if (seed[cursor_x][cursor_y]==1) {
+					printw(" ");
+				} else {
+					printw("X");
+				}
 				//update_seed();
+				seed[cursor_x][cursor_y] = seed[cursor_x][cursor_y]==1 ? 0 : 1;
 				refresh();
 				break;
 		}
-		move(cursorlocx, cursorlocy);
+		move(cursor_x, cursor_y);
         refresh();
 	}
+	clear();
+	refresh();
+	move(0,0);
+	printw("Seed state saved. Press ENTER to start the simulation.");
+
+	// draw_state()
+	getch();
+	move(0,0);
+	for (int i=0; i<WINDMAXY; i++) {
+		for (int j=0; j<WINDMAXX; j++) {
+			if (seed[i][j]==1) {
+				printw("X");
+			} else {
+				printw(" ");
+			}
+		}
+	}
+	refresh();
+	// get_next_state
+	getch();
 
     endwin();           /* End curses mode        */
 
